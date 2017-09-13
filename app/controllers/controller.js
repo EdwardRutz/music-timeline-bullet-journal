b99ddb4c0f616b3981392983fa0f5bc2c64a5841
Edward R <edwardrz@users.noreply.github.com>
2017-09-13 14:44:54 -0500

    Created controller.js

---
+++ app/controllers/controller.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("controller.js");
+
+app.controller("controller", function($scope, eventFactory) {
+
+$scope.eventArray = [];
+	
+	eventFactory.getEvent()  //define function
+	.then(function(eventCollection) {
+		$scope.eventArray = eventCollection[0];//binds data to scope
+		console.log("$scope.eventArray", $scope.eventArray);
+	});
+});
