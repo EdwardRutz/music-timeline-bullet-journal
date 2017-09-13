d278650bfd2d2bc0d123fca3a530c9e24b952c36
Edward R <edwardrz@users.noreply.github.com>
2017-09-13 13:44:24 -0500

    Changed partial name to homeView.

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 console.log("app.js");
 
 var app = angular.module("capstoneApp", ["ngRoute"]);
+
 app.config(["$routeProvider",
 	function($routeProvider) {
-		$routeProvider.
-		when("/", {
+		$routeProvider
+		.when("/", {
 			templateUrl: "partials/homeView.html",
-			controller: "homeViewCtrl",
+			controller: "controller",
 		})
 		.otherwise("/");
 }]);
